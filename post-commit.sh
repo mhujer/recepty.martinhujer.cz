6a5c45199893d3f32c4247da3f29cd715ab8ddc9
Martin Hujer <mhujer@gmail.com>
2014-12-20 00:00:50 +0100

    postcommit

--- post-commit.sh
+++ post-commit.sh
@@ -2,9 +2,9 @@
 
 rm -rf output_prod
 php /c/xampp/php/sculpin.phar generate --env=prod
-rm -rf /c/xampp/recepty.martinujer.cz-pages/*
-cp -R /c/xampp/htdocs/recepty.martinujer.cz/output_prod/* /c/xampp/htdocs/recepty.martinujer.cz-pages/
-cd /c/xampp/htdocs/recepty.martinujer.cz-pages/
+rm -rf /c/xampp/htdocs/recepty.martinhujer.cz-pages/*
+cp -R /c/xampp/htdocs/recepty.martinhujer.cz/output_prod/* /c/xampp/htdocs/recepty.martinhujer.cz-pages/
+cd /c/xampp/htdocs/recepty.martinhujer.cz-pages/
 git add -A
 git commit -a -m "Update"
 git push origin gh-pages:gh-pages
