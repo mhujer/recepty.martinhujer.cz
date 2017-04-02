634ae545cb6443c2fc959f6ce5029bef9f7e0a53
Martin Hujer <mhujer@gmail.com>
2017-04-02 08:53:32 +0200

    sync with changes in latest Sculpin

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,5 +4,5 @@ rm -rf output_prod
 rm -rf docs
 php ../sculpin/bin/sculpin generate --env=prod
 mkdir docs
-cp -R /c/xampp/htdocs/recepty.martinhujer.cz/output_prod/* /c/xampp/htdocs/recepty.martinhujer.cz/docs
+cp -R /c/devweb/htdocs/recepty.martinhujer.cz/output_prod/* /c/devweb/htdocs/recepty.martinhujer.cz/docs
 git add -A
