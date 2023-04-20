645f98aa96139a21112a2a035bcd292df726dbf2
Martin Hujer <mail@martinhujer.cz>
2023-04-20 13:41:50 +0200

    cleanup

--- pre-commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm -rf output_prod
-rm -rf docs
-php ../sculpin/bin/sculpin generate --env=prod
-mkdir docs
-cp -R /c/dev/htdocs/recepty.martinhujer.cz/output_prod/* /c/dev/htdocs/recepty.martinhujer.cz/docs
-git add -A
